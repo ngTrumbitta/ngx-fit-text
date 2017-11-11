1f9daff425eeb4872a97710786ee72caebe359e3
William Ghelfi <trumbitta@gmail.com>
2017-11-11 23:12:23 +0100

    Add directive to its module

--- src/ngx-fit-text.module.ts
+++ src/ngx-fit-text.module.ts
@@ -1,22 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { NgxFitTextDirective } from './directives/ngx-fit-text.directive';
+
 @NgModule({
-    declarations: [
-
-    ],
-    exports: [
-
-    ],
-    imports: [
-        CommonModule
-    ]
+  declarations: [NgxFitTextDirective],
+  exports: [NgxFitTextDirective],
+  imports: [CommonModule]
 })
 export class NgxFitTextModule {
-    static forRoot() {
-        return {
-            ngModule: NgxFitTextModule,
-            providers: []
-        };
-    }
+  static forRoot() {
+    return {
+      ngModule: NgxFitTextModule,
+      providers: []
+    };
+  }
 }
